5a582d522ac1d307292f928f077d5e3678cd6d55
Kacper Topolski <kacpertopolski@op.pl>
2024-12-21 00:27:31 +0100

    Day 6 (2024)

---
+++ 2024/src/bin/day-06-part1.rs
@@ -0,0 +1,85 @@
+use std::collections::HashSet;
+use std::io;
+use std::io::prelude::*;
+
+pub trait Board {
+    fn find(&self, ch: char) -> (usize, usize);
+    fn at(&self, i: usize, j: usize) -> Option<char>;
+    fn ati(&self, i: i64, j: i64) -> Option<char> {
+        if let (Ok(i), Ok(j)) = (usize::try_from(i), usize::try_from(j)) {
+            self.at(i, j)
+        }
+        else {
+            None
+        }
+    }
+    fn ati_p(&self, (i, j): (i64, i64)) -> Option<char> {
+        self.ati(i, j)
+    }
+}
+
+impl Board for Vec<Vec<char>> {
+    fn find(&self, ch: char) -> (usize, usize) {
+        for i in 0..self.len() {
+            for j in 0..self[i].len() {
+                if self[i][j] == ch {
+                    return (i, j);
+                }
+            }
+        }
+        panic!()
+    }
+    fn at(&self, i: usize, j: usize) -> Option<char> {
+        self.get(i).and_then(|v| v.get(j)).cloned()
+    }
+}
+
+fn add((a, b): (i64, i64), (c, d): (i64, i64)) -> (i64, i64) {
+    (a + c, b + d)
+}
+
+fn solve(input: Vec<String>) -> i64 {
+    let board: Vec<Vec<char>> = input.iter().map(|s| s.chars().collect()).collect();
+
+    let (i, j) = board.find('^');
+    let mut pos = (i as i64, j as i64);
+
+    let dirs: Vec<(i64, i64)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mut dir = 0;
+
+    let mut vis = HashSet::new();
+
+    while let Some(_) = board.ati_p(pos) {
+        vis.insert(pos);
+
+        while board.ati_p(add(pos, dirs[dir])) == Some('#') {
+            dir = (dir + 1) % 4;
+        }
+
+        pos = add(pos, dirs[dir]);
+    }
+
+    let r = vis.len() as i64;
+    dbg!(r)
+}
+
+fn main() {
+    solve(io::stdin().lock().lines().flatten().collect());
+}
+
+#[test]
+fn sample() {
+    let txt = "\
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+    let ans = solve(txt.split('\n').map(|x| x.to_string()).collect());
+    assert_eq!(ans, 41);
+}
